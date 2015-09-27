3ee1696dac5a29875f027e0d4c835a67c0015fff
kimply <jackwarpten@hotmail.com>
2015-09-27 02:35:14 +0000

    fix some style

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -22,6 +22,10 @@ class PagesController < ApplicationController
    def family
       @questions = Question.where(category: "Family").paginate(:page => params[:page], :per_page => 5)
    end
+   
+   def education
+      @questions = Question.where(category: "Education").paginate(:page => params[:page], :per_page => 5)
+   end
    def answered
       @questions = Question.joins(:answers).paginate(:page => params[:page], :per_page => 5)
    end
